d0304495a6b4de7728edb7a2e1093ee996b0fe16
U1710099 <gulbakhor_mirzalieva@mail.ru>
2019-04-24 15:24:30 +0500

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,70 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::post('create', function( \Illuminate\Http\Request $request,
+\Illuminate\Validation\Factory $validator)
+{
+$validation = $validator->make($request->all(), [
+'title' => 'required|min:5',
+'content' => 'required|min:10'
+]);
+if (validation->fails())
+{
+    return redirect()->back()->withErrors($validation);
+
+}
+
+return redirect()
+->route('admin.index')
+->with('info', 'Post created, Title: ' . $request->input('title'));
+})->name('admin.create');
+
+
+Route::post('edit', function(\Illuminate\Http\Request $request , \Illuminate\Validation\Factory $validator) { $validation = $validator ->make($request ->all(), ['title' => 'required|min:5', 'content' => 'required|min:10' ]); if ($validation ->fails()) { return redirect()->back()->withErrors($validation); } return redirect() ->route('admin.index') ->with('info', 'Post edited , new Title: ' . $request ->input('title')); })->name('admin.update')
+
+
+
+
+Route::get('/', [
+    'uses' => 'PostController@getIndex',
+    'as' => 'blog.index'
+]);
+Route::get('post/{id}', [
+    'uses' => 'PostController@getPost',
+    'as' => 'blog.post'
+]);
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', [
+        'uses' => 'PostController@getAdminIndex',
+        'as' => 'admin.index'
+    ]);
+    Route::get('create', [
+        'uses' => 'PostController@getAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::post('create', [
+        'uses' => 'PostController@postAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::get('edit/{id}', [
+        'uses' => 'PostController@getAdminEdit',
+        'as' => 'admin.edit'
+    ]);
+    Route::post('edit', [
+        'uses' => 'PostController@postAdminUpdate',
+        'as' => 'admin.update'
+    ]);
 });
